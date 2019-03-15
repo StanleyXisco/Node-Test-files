92c441f85017ab5543eb18836c5e995bf62b86b8
xisco <gistextra@gmail.com>
2019-03-15 12:57:22 +0100

    'Updated'

---
+++ script.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+stanley = () =>{
+	fs.readFile('./santa.txt', (error, data)=>{
+		console.time('Time Machine');
+		const directions = data.toString();
+		const directionsArrays = directions.split('');
+		const answer = directionsArrays.reduce( (acc, currentValue) => {
+			if ( currentValue === '(') {
+				return acc += 1;
+			} else if (currentValue === ')') {
+				return acc	-= 1;
+			}
+		}, 0)
+		console.timeEnd('Time Machine');
+		console.log('Santa\'s Floor', answer);
+	});
+}
+
+// stanley();
+
+
+// When does stanta enter the basement
+
+chidera = () => {
+	fs.readFile('./santa.txt', (error, data) =>  {
+		const directions = data.toString();
+		const directionsArrays = directions.split('');
+		let acc = 0;
+		let counter = 0;
+		const answer = directionsArrays.some((currentValue) =>{
+			if (currentValue === '('){
+				acc += 1;
+			} else if (currentValue === ')'){
+				acc -= 1;
+			}
+			counter ++ ;
+			return acc < 0;
+		})
+		console.log('Santa entered the basement at floor', counter);
+	})
+}
+
+chidera();
